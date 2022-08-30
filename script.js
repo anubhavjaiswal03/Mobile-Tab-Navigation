925bd15cb2b6ccdaf10f092955c399448d6aa194
Anubhav Jaiswal <36488000+anubhavjaiswal03@users.noreply.github.com>
2022-08-30 22:07:21 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const navList = document.querySelectorAll('.phone nav ul li');
+const contentList = document.querySelectorAll('.content');
 
+navList.forEach((navItem, idx) => {
+	navItem.addEventListener('click', () => {
+		const activeNav = document.querySelector('.active');
+		const activeContent = document.querySelector('.content.show');
+
+		activeNav.classList.toggle('active');
+		navItem.classList.toggle('active');
+
+		activeContent.classList.toggle('show');
+		contentList[idx].classList.toggle('show');
+	});
+});
